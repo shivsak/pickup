08d0bf33aa8bc9e291f8d3dcbe2c3bfe4fd9efda
Shiv Sakhuja <shivsak.s@gmail.com>
2016-02-05 21:24:56 -0500

    UI Changes to Index, icons added

--- app/models/game.rb
+++ app/models/game.rb
@@ -5,4 +5,59 @@ class Game < ActiveRecord::Base
     self.date.strftime("%B, %Y")
   end
 
+  def get_black_icon
+    if self.game_type.downcase == 'football'
+      '/images/black_icons/football.png'
+    elsif self.game_type.downcase == 'soccer'
+      '/images/black_icons/soccer.png'
+    elsif self.game_type.downcase == 'basketball'
+      '/images/black_icons/basketball.png'
+    elsif self.game_type.downcase == 'archery'
+      '/images/black_icons/archery.png'
+    elsif self.game_type.downcase == 'badminton'
+      '/images/black_icons/badminton.png'
+    elsif self.game_type.downcase == 'tennis'
+      '/images/black_icons/tennis.png'
+    elsif self.game_type.downcase == 'squash'
+      '/images/black_icons/squash.png'
+    elsif self.game_type.downcase == 'swim'
+      '/images/black_icons/swim.png'
+    elsif self.game_type.downcase == 'baseball'
+      '/images/black_icons/baseball.png'
+    elsif self.game_type.downcase == 'cycle'
+      '/images/black_icons/cycle.png'
+    else
+
+    end
+  end
+
+  def get_white_icon
+    
+      if self.game_type.downcase == 'football'
+        '/images/white_icons/football.png'
+      elsif self.game_type.downcase == 'soccer'
+        '/images/white_icons/soccer.png'
+      elsif self.game_type.downcase == 'basketball'
+        '/images/white_icons/basketball.png'
+      elsif self.game_type.downcase == 'archery'
+        '/images/white_icons/archery.png'
+      elsif self.game_type.downcase == 'badminton'
+        '/images/white_icons/badminton.png'
+      elsif self.game_type.downcase == 'tennis'
+        '/images/white_icons/tennis.png'
+      elsif self.game_type.downcase == 'squash'
+        '/images/white_icons/squash.png'
+      elsif self.game_type.downcase == 'swim'
+        '/images/white_icons/swim.png'
+      elsif self.game_type.downcase == 'baseball'
+        '/images/white_icons/baseball.png'
+      elsif self.game_type.downcase == 'cycle'
+        '/images/white_icons/cycle.png'
+      else
+
+      end
+
+
+  end
+
 end
